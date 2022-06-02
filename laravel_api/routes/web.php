4004fa24f3eef946f6932c39f7885b516d30214c
TrandinhdatAgl <tran_dinh_dat@allgrow-labo.jp>
2022-06-02 17:46:53 +0700

    spatie 1

--- laravel_api/routes/web.php
+++ laravel_api/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/role', function () {
+    $role = Role::create(['name' => 'writer']);
+    $permission = Permission::create(['name' => 'edit articles']);
+});
+
+Route::get('/g', function () {
+    $role = Role::find(1);
+    $permission = Permission::find(1);
+    // $role->givePermissionTo($permission);
+    // $permission->assignRole($role);
+});
+Route::get('/u', function() {
+    $user = User::find(8);
+    // $user->givePermissionTo('edit articles');
+    // $user->assignRole('writer');
+    // dd($user->getRoleNames());
+    // dd($all_roles_in_database = Role::all()->pluck('name'));
+    dd($permissions = $user->getPermissionNames());
+    dd($user->can('edit articles'));
 });
